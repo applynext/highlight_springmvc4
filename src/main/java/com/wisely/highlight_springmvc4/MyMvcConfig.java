d7e5a2c76bcc97cfef22a958061874fcf9577ebb
dragon <applynext@163.com>
2018-02-07 18:24:57 +0800

    Interceptor

--- src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
+++ src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.wisely.highlight_springmvc4.interceptor.DemoInterceptor;
+
 @Configuration
-@EnableWebMvc
+@EnableWebMvc // 1 开户SpringMVC支持
 @ComponentScan("com.wisely.highlight_springmvc4")
-public class MyMvcConfig {
+// 2 继承该类，重写其方法对SpringMVC配置
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -19,4 +25,21 @@ public class MyMvcConfig {
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 3 addResourceLocations指本地资源放置路径，addResourceHandler指对外暴露的访问路径
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+	}
+
+	@Bean // 1 配置拦截器的Bean
+	public DemoInterceptor demoInterceptor() {
+		return new DemoInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(demoInterceptor());
+	}
+
 }
