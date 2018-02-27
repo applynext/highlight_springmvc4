88f07561fb6ac677f52a6424629e3bb6835a56ea
dragon <applynext@163.com>
2018-02-27 17:23:12 +0800

    其它配置

--- src/main/java/com/wisely/highlight_springmvc4/HelloController.java
+++ src/main/java/com/wisely/highlight_springmvc4/HelloController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // 1
 public class HelloController {
-	@RequestMapping("/index") // 2 配置URL和方法之间的映射
-	public String hello() {
-		return "index"; // 3
-	}
+	// 可以以配置类中重写addViewControllers()方法简化配置
+//	@RequestMapping("/index") // 2 配置URL和方法之间的映射
+//	public String hello() {
+//		return "index"; // 3
+//	}
 }
--- src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
+++ src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -41,5 +43,17 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(demoInterceptor());
 	}
+	
+	// 配置页面转向的时候的简化写法，由此在控制器当中无需配置URL和方法之间的映射
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("/index");
+	}
 
+	// 通过重写该方法设定不忽略路径"."后面的参数
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		configurer.setUseSuffixPatternMatch(false);
+	}
+	
 }
